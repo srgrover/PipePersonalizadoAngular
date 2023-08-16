a2dde1744dbc539ab0ce1eed47491f2f268a6b15
jmoya2 <jmoya2@ES-9XPHCL3>
2023-08-16 13:18:37 +0200

    Declare user variable with data. Create get for users

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { User } from '../Models/User';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
+  users: User[] = [
+    { name: "Jack", surname: "Sparrow", email: "captainJackSparrow@pirates.com", age: 28 },
+    { name: "Will", surname: "Turner", email: "willturner@pirates.com", age: 21 },
+    { name: "Davy", surname: "Jones", email: "davyjones@pirates.com", age: 110 },
+    { name: "Hector", surname: "Barbossa", email: "hectorBarbossa@pirates.com", age: 58 },
+    { name: "Elizabeth", surname: "Swann", email: "elizabethswann@pirates.com", age: 20 }
+  ];
 
   constructor() { }
+
+  getUsers(): User[] {
+    return this.users;
+  }
 }
