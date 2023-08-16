beb92a5041ca825f14c71ab48b3da7a38d9c85a1
jmoya2 <jmoya2@ES-9XPHCL3>
2023-08-16 14:24:07 +0200

    Create pipe for sort by condition

---
+++ src/app/pipes/sortString.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+    name: 'sortString'
+})
+export class sortStringPipe implements PipeTransform {
+    transform(value: any[], sortBy: string, sortOrder: 'asc' | 'desc'): any[] {
+        if (!value || !Array.isArray(value)) {
+            return value;
+        }
+
+        return value.sort((a, b) => {
+            const aValue = a[sortBy].toLowerCase();
+            const bValue = b[sortBy].toLowerCase();
+            return sortOrder === 'asc' ? aValue.localeCompare(bValue) : bValue.localeCompare(aValue);
+        });
+    }
+}
